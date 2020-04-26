5d315d105e2c9646af5f064e06bf62112cadd0d2
Den Williams <work@denwilliams.net>
2020-04-26 15:57:54 +1000

    fix topic deconstruct

--- main.js
+++ main.js
@@ -17,31 +17,36 @@ client.init({
 const service = mqttusvc.create();
 
 service.on("message", (topic, data) => {
-  console.log("message", topic);
-  if (!topic.startsWith("~/set/")) return;
+  try {
+    console.log("message", topic);
+    if (!topic.startsWith("~/set/")) return;
 
-  const [_, lifxId, action] = topic.split("/");
-  console.info("SET DEVICE", lifxId, action, data);
-  const light = client.light(lifxId);
+    const [, , lifxId, action] = topic.split("/");
+    console.info("SET DEVICE", lifxId, action, data);
+    const light = client.light(lifxId);
 
-  if (action === "off") return light.off(5000);
-  if (action === "on") return light.on(5000);
+    if (action === "off") return light.off(5000);
+    if (action === "on") return light.on(5000);
 
-  const request = JSON.parse(data);
+    const request = JSON.parse(data);
 
-  if (!request) return light.off(5000);
-  if (request.brightness === 0) return light.off(request.duration || 5000);
-  if (request.color) light.colorRgbHex(request.color, request.duration || 2000);
-  else if (request.temp)
-    light.color(
-      0,
-      0,
-      request.brightness,
-      request.temp,
-      request.duration || 2000
-    );
+    if (!request) return light.off(5000);
+    if (request.brightness === 0) return light.off(request.duration || 5000);
+    if (request.color)
+      light.colorRgbHex(request.color, request.duration || 2000);
+    else if (request.temp)
+      light.color(
+        0,
+        0,
+        request.brightness,
+        request.temp,
+        request.duration || 2000
+      );
 
-  light.on(request.duration || 5000);
+    light.on(request.duration || 5000);
+  } catch (err) {
+    console.error(`Unable to handle message. topic=${topic} data=${data}`);
+  }
 });
 
 service.subscribe("~/set/#");
